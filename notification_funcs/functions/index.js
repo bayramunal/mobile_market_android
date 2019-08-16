9ac78f3bd94ab66b5c462bef67f303d61cb36965
bayramunal <bayramunal@users.noreply.github.com>
2019-08-16 13:40:12 +0300

    application retrieve the notification when app in the minimized or closed state

--- notification_funcs/functions/index.js
+++ notification_funcs/functions/index.js
@@ -1,41 +1,45 @@
-'use strict'
+"use strict";
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
-exports.sendNotification = functions.database.ref('/notifications/{user_id}/notification_id').onWrite((change, context) => {
+exports.sendNotification = functions.database
+  .ref("/notifications/{user_id}/{notification_id}")
+  .onWrite((change, context) => {
+    const user_id = context.params.user_id;
+    const notification = context.params.notification;
 
-	const user_id = context.params.user_id;
-	const notification = context.params.notification;
+    console.log("we have a notification to send to : ", user_id);
 
-	console.log('we have a notification to sen to : ', user_id);
-	
-	if (!context.data.val()) {
-		return console.log('a notitification has been deleted from the database : ' + notification_id);
-	}
-	
-	const deviceToken = admin.database().ref(`/_users/${user_id}/device_token`).once('value');
-	
-	return deviceToken.then(result => {
-		
-		const token_id = result.val();
-		
-		const payload = {
-			notification : {
-				title : "friend request",
-				body : "you have received a new friend request",
-				icon : "default
-			}
-		};
-	
-		return admin.messaging().sendToDevice(token_id ,payload).then(response => {
-			
-			console.log('this was the notification feature');
-			
-		});
-		
-	});
-	
-	
-});
+    if (!change.after.val()) {
+      return console.log(
+        "a notification has been deleted from the database : ",
+        notification
+      );
+    }
+
+    const deviceToken = admin
+      .database()
+      .ref(`/_users/${user_id}/device_token`)
+      .once("value");
+
+    return deviceToken.then(result => {
+      const token_id = result.val();
+
+      const payload = {
+        notification: {
+          title: "Friend Request",
+          body: "You have a received a new friend request!",
+          icon: "default"
+        }
+      };
+
+      return admin
+        .messaging()
+        .sendToDevice(token_id, payload)
+        .then(response => {
+          return console.log("This was the notification Feature");
+        });
+    });
+  });
